cf6280169937c997fcafe12709bfa08bba7e8a61
Nube974 <lucianoboyer97@hotmail.com>
2025-07-25 10:34:54 +0400

    Musique de début et de boss

--- Assets/Audio/Musiques/AudioManager.cs
+++ Assets/Audio/Musiques/AudioManager.cs
@@ -5,6 +5,7 @@ public class AudioManager : MonoBehaviour
 	public AudioClip[] playlist;
 	public AudioSource audioSource;
 	private int musicIndex = 0;
+	public bool nextMusic = false;
 	
     void Start()
     {
@@ -22,10 +23,24 @@ public class AudioManager : MonoBehaviour
 		}
     }
 	
-	void PlayNextSong()
+	public void PlayNextSong()
 	{
+		print("musique suivante");
 		musicIndex = (musicIndex + 1) % playlist.Length;
 		audioSource.clip = playlist[musicIndex];
 		audioSource.Play();
+	}
+	void OnCollisionEnter2D(Collision2D collision)
+    {
+		print(collision.gameObject.tag);
+		//PlayNextSong();
+        if (collision.gameObject.CompareTag("MusicTrigger"))
+        {
+            // Contact principal
+			if (!nextMusic){
+				PlayNextSong();
+				nextMusic = true;
+			}
+        }
 	}
 }
